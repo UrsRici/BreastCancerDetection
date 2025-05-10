fe3d211094c9a6447b6db8cd0c23bfab59c9e1b2
UrsRici <ursrici.ru@gmail.com>
2025-05-10 18:50:27 +0300

    information page, background image, start page, optimizations

--- BrestCancerDetection/Properties/Resources.Designer.cs
+++ BrestCancerDetection/Properties/Resources.Designer.cs
@@ -63,6 +63,18 @@ namespace BrestCancerDetection.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap background
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap blue_information_button {
             get {
                 object obj = ResourceManager.GetObject("blue-information-button", resourceCulture);
